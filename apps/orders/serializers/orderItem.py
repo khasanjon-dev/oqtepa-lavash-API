fbf35a0375cfd68ed8f145f5d79d9fa6c4f2a227
Khasanjon <khasanjon.eng@gmail.com>
2023-09-23 22:38:03 +0500

    update some files

---
+++ apps/orders/serializers/orderItem.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from orders.models import OrderItem
+
+
+class OrderItemSerializer(ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
