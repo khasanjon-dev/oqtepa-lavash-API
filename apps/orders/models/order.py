a09725836e68499137e23d3aa8c185e90692cd2e
Khasanjon <khasanjon.eng@gmail.com>
2023-10-06 23:22:35 +0500

    update some files

--- apps/orders/models/order.py
+++ apps/orders/models/order.py
@@ -1,13 +1,20 @@
-from django.db.models import ForeignKey, SET_NULL, Model, TextField, DateTimeField, BooleanField
+from django.db.models import ForeignKey, SET_NULL, Model, TextField, DateTimeField, BooleanField, TextChoices, CharField
 
 from products.models import Product
 from users.models import User
 
 
 class Order(Model):
+    class ReceptionType(TextChoices):
+        DELIVERY = 'delivery', 'delivery'
+        PICKUP = 'pickup', 'pickup'
+
     address = TextField()
     status = BooleanField(default=False)
     date = DateTimeField(auto_now_add=True)
+
+    # choices
+    reception_type = CharField(max_length=8, choices=ReceptionType.choices)
 
     # relationships
     product = ForeignKey(Product, SET_NULL, null=True)
