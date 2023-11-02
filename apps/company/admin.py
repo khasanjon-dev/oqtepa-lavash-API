7dae9b12ce97224121e56e144fa061991013efc9
khasanjon <khasanjon.dev@gmail.com>
2023-11-03 00:16:20 +0500

    add some action

--- apps/company/admin.py
+++ apps/company/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from company.models import Settings, About, Phone, Social, Branch, Location
+
+
+@admin.register(Settings)
+class SettingsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Social)
+class SocialAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    pass
