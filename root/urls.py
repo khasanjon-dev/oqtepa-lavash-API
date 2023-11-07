01e0befad102fd5cc835d7c4d27d9a91b966fe7d
khasanjon <khasanjon.dev@gmail.com>
2023-11-07 11:38:19 +0500

    update api permissions

--- root/urls.py
+++ root/urls.py
@@ -3,14 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework.permissions import IsAdminUser, AllowAny
 
 from root import settings
-from root.settings import DEBUG
 
-permissions = [AllowAny]
-if DEBUG:
-    permissions = [IsAdminUser]
+# permissions = [AllowAny]
+# if DEBUG:
+#     permissions = [IsAdminUser]
 schema_view = get_schema_view(
     openapi.Info(
         title="Oqtepa Lavash API",
@@ -20,7 +18,7 @@ schema_view = get_schema_view(
         # license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=permissions,
+    # permission_classes=permissions,
 )
 
 urlpatterns = [
