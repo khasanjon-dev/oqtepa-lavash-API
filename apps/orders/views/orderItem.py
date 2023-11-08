ac8a5c0c3f832d3eba759a278ce3961804e6c122
khasanjon <khasanjon.dev@gmail.com>
2023-11-08 22:29:22 +0500

    update order API

--- apps/orders/views/orderItem.py
+++ apps/orders/views/orderItem.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
@@ -10,3 +11,13 @@ class OrderItemViewSet(CreateModelMixin, GenericViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
     permission_classes = (IsAuthenticated,)
+
+    @action(methods=['get'], detail=True)
+    def orders(self, request):
+        """
+        ```
+        order lar listini olish uchun
+        ```
+        """
+        orders = request.user.order_set
+        orders_ids = orders.values_list('order', flat=True)
