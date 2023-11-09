30fcfbb82759a09e7a7e05ef223d8dd9df9c71e6
khasanjon <khasanjon.dev@gmail.com>
2023-11-10 00:29:48 +0500

    update some files

--- apps/orders/views/orderItem.py
+++ apps/orders/views/orderItem.py
@@ -11,8 +11,8 @@ class OrderItemViewSet(CreateModelMixin, GenericViewSet):
     serializer_class = OrderItemSerializer
     permission_classes = (IsAuthenticated,)
 
-    # @action(methods=['get'], detail=False)
-    # def order(self, request):
-    #     queryset = OrderItem.objects.filter(order__customer=request.user)
-    #     serializer = self.get_serializer(queryset, many=True)
-    #     return Response(serializer.data)
+    @action(methods=['get'], detail=False)
+    def order(self, request):
+        queryset = OrderItem.objects.filter(order__customer=request.user)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
