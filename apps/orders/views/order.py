2c298ca17bcd2d07c804b7977cdf8c3c69ab2f19
khasanjon <khasanjon.dev@gmail.com>
2023-11-09 23:08:02 +0500

    update some files

--- apps/orders/views/order.py
+++ apps/orders/views/order.py
@@ -1,11 +1,10 @@
+from orders.models import Order
+from orders.serializers.order import OrderSerializer
 from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-
-from orders.models import Order
-from orders.serializers.order import OrderSerializer
 
 
 class OrderViewSet(CreateModelMixin, GenericViewSet):
