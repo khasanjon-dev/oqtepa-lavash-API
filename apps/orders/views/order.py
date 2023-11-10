fb34496dbe480065ad9b543076d5eac7d449cb4c
khasanjon <khasanjon.dev@gmail.com>
2023-11-10 11:14:44 +0500

    ready for development

--- apps/orders/views/order.py
+++ apps/orders/views/order.py
@@ -1,12 +1,11 @@
+from orders.models import Order
+from orders.serializers.order import OrderSerializer
+from orders.utils.pagination import CustomPageNumberPagination
 from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-
-from orders.models import Order
-from orders.serializers.order import OrderSerializer
-from orders.utils.pagination import CustomPageNumberPagination
 
 
 class OrderViewSet(CreateModelMixin, GenericViewSet):
