877a4b12a1966831a95b2062396b548878432d9d
khasanjon <khasanjon.dev@gmail.com>
2023-11-19 21:28:37 +0500

    update some files

--- apps/products/views/product.py
+++ apps/products/views/product.py
@@ -1,8 +1,4 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from products.filter import ProductFilterSet
-from products.models import Product
-from products.serializers import ProductSerializer
-from products.serializers.product import BasketModelSerializer, NoneSerializer
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
@@ -11,6 +7,11 @@ from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+
+from products.filter import ProductFilterSet
+from products.models import Product
+from products.serializers import ProductSerializer
+from products.serializers.product import BasketModelSerializer, NoneSerializer
 from users.models import Favorite
 from users.models.addition import Basket
 
