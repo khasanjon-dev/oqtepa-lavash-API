1339192396a5de342fba2079b43a4fe6a896e74c
khasanjon <khasanjon.dev@gmail.com>
2023-11-20 12:26:58 +0500

    update some files

--- apps/users/views/auth.py
+++ apps/users/views/auth.py
@@ -1,12 +1,17 @@
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.settings import api_settings
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenViewBase
+
 from users.serializers.jwt_auth import MyTokenObtainPairSerializer
 
 
 class LoginView(TokenObtainPairView):
     """
-    ## 1. bundan avval register ga yuboriladi
-    ## login qilish uchun
+    access va refresh token olish uchun
+
+    ```
+    1. bundan avval register ga yuboriladi
+    ```
     ```
     {
         "phone": "901001010",
@@ -16,3 +21,13 @@ class LoginView(TokenObtainPairView):
     """
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
+
+class CustomTokenRefreshView(TokenViewBase):
+    """
+    refresh token olish uchun
+
+    ```
+    """
+
+    _serializer_class = api_settings.TOKEN_REFRESH_SERIALIZER
